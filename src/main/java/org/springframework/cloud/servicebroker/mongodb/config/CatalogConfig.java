bbb7221204fbe41c12d18c2039bb4884fd8d6c03
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2018-05-26 01:16:36 +0530

    multiple plans update

--- src/main/java/org/springframework/cloud/servicebroker/mongodb/config/CatalogConfig.java
+++ src/main/java/org/springframework/cloud/servicebroker/mongodb/config/CatalogConfig.java
@@ -1,11 +1,10 @@
 package org.springframework.cloud.servicebroker.mongodb.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import static org.springframework.cloud.servicebroker.mongodb.config.CatalogConfig.ServicePlanIdentifier.*;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.servicebroker.model.Catalog;
 import org.springframework.cloud.servicebroker.model.Plan;
 import org.springframework.cloud.servicebroker.model.ServiceDefinition;
@@ -14,63 +13,143 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CatalogConfig {
-	
-	@Bean
-	public Catalog catalog() {
-		return new Catalog(Collections.singletonList(
-				new ServiceDefinition(
-						"mongodb01-on-demand-service-broker",
-						"mongodb01",
-						"A PKS MongoDB on-demand service broker implementation",
-						true,
-						false,
-						Collections.singletonList(
-								new Plan("mongo01-plan",
-										"default",
-										"This is a default mongo plan.  All services are created equally.",
-										getPlanMetadata())),
-						Arrays.asList("mongodb", "document"),
-						getServiceDefinitionMetadata(),
-						null,
-						null)));
-	}
-	
-/* Used by Pivotal CF console */
 
-	private Map<String, Object> getServiceDefinitionMetadata() {
-		Map<String, Object> sdMetadata = new HashMap<>();
-		sdMetadata.put("displayName", "MongoDB");
-		sdMetadata.put("imageUrl", "http://info.mongodb.com/rs/mongodb/images/MongoDB_Logo_Full.png");
-		sdMetadata.put("longDescription", "MongoDB Service");
-		sdMetadata.put("providerDisplayName", "Pivotal");
-		sdMetadata.put("documentationUrl", "https://github.com/srinivasa-vasu/cloudfoundry-service-broker");
-		sdMetadata.put("supportUrl", "https://github.com/srinivasa-vasu/cloudfoundry-service-broker");
-		return sdMetadata;
-	}
-	
-	private Map<String,Object> getPlanMetadata() {
-		Map<String,Object> planMetadata = new HashMap<>();
-		planMetadata.put("costs", getCosts());
-		planMetadata.put("bullets", getBullets());
-		return planMetadata;
-	}
+    public static final String COSTS = "costs";
+    public static final String BULLETS = "bullets";
+    public static final String AMOUNT = "amount";
+    public static final String UNIT = "unit";
+    public static final String STORAGE = "storage";
+    public static final String REPLICAS = "replicas";
+    public static final String CURRENCY = "usd";
 
-	private List<Map<String,Object>> getCosts() {
-		Map<String,Object> costsMap = new HashMap<>();
-		
-		Map<String,Object> amount = new HashMap<>();
-		amount.put("usd", 0.0);
-	
-		costsMap.put("amount", amount);
-		costsMap.put("unit", "MONTHLY");
-		
-		return Collections.singletonList(costsMap);
-	}
-	
-	private List<String> getBullets() {
-		return Arrays.asList("On-demand MongoDB server",
-				"128 Mi Storage (enforced)",
-				"40 concurrent connections (not enforced)");
-	}
-	
+    enum ServicePlanIdentifier {
+        D, G, P
+    }
+
+    public enum ServicePlan {
+
+        DEFAULT("default", "default", "This is a default mongo free plan", true, true, getPlanMetadata(D)),
+        GOLD("gold", "gold", "This is a paid mongo plan", false, true, getPlanMetadata(G)),
+        PLATINUM("platinum", "platinum", "This is a paid premium mongo plan", false, true, getPlanMetadata(P));
+
+        ServicePlan(String id, String name, String description, boolean free, boolean bindable,
+                    Map<String, Object> metadata) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.free = free;
+            this.bindable = bindable;
+            this.metadata = metadata;
+        }
+
+        private String id;
+        private String name;
+        private String description;
+        private Map<String, Object> metadata;
+        private boolean free;
+        private boolean bindable;
+
+        private static Map<String, Object> getPlanMetadata(ServicePlanIdentifier plan) {
+            Map<String, Object> planMetadata = new HashMap<>();
+            Map<String, Object> costsMap = new HashMap<>();
+            List<String> bulletsList = null;
+            switch (plan) {
+                case D:
+                    costsMap.put(AMOUNT, new HashMap<String, Object>() {
+                        {
+                            put(CURRENCY, 0.0);
+                        }
+                    });
+                    costsMap.put(UNIT, "MONTHLY");
+                    costsMap.put(STORAGE, "128Mi");
+                    costsMap.put(REPLICAS, "1");
+                    bulletsList = Arrays.asList("128Mi Storage (enforced)", "Single instance");
+                    break;
+                case G:
+                    costsMap.put(AMOUNT, new HashMap<String, Object>() {
+                        {
+                            put(CURRENCY, 100.0);
+                        }
+                    });
+                    costsMap.put(UNIT, "MONTHLY");
+                    costsMap.put(STORAGE, "1Gi");
+                    costsMap.put(REPLICAS, "3");
+                    bulletsList = Arrays.asList("1Gi Storage (enforced)", "3 instances");
+                    break;
+                case P:
+                    costsMap.put(AMOUNT, new HashMap<String, Object>() {
+                        {
+                            put(CURRENCY, 500.0);
+                        }
+                    });
+                    costsMap.put(UNIT, "MONTHLY");
+                    costsMap.put(STORAGE, "10Gi");
+                    costsMap.put(REPLICAS, "5");
+                    bulletsList = Arrays.asList("10Gi Storage (enforced)", "5 instances");
+                    break;
+            }
+            planMetadata.put(COSTS, Collections.singletonList(costsMap));
+            planMetadata.put(BULLETS, bulletsList);
+            return planMetadata;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public Map<String, Object> getMetadata() {
+            return metadata;
+        }
+
+        public boolean isFree() {
+            return free;
+        }
+
+        public boolean isBindable() {
+            return bindable;
+        }
+    }
+
+    @Value("${service.id:mongodb}")
+    private String serviceId;
+
+    @Bean
+    public Catalog catalog() {
+
+        List<Plan> planList = new ArrayList<>();
+        for (ServicePlan plan : ServicePlan.values()) {
+            planList.add(new Plan(serviceId + plan.getId(), plan.getName(),
+                    plan.getDescription(), plan.getMetadata(), plan.isFree(), plan.isBindable()));
+        }
+
+        return new Catalog(Collections.singletonList(new ServiceDefinition(serviceId,
+                serviceId, "A PKS MongoDB on-demand service broker implementation",
+                true, false, planList, Arrays.asList("mongodb", "document"),
+                getServiceDefinitionMetadata(), null, null)));
+    }
+
+    /* Used by Pivotal CF console */
+
+    private Map<String, Object> getServiceDefinitionMetadata() {
+        Map<String, Object> sdMetadata = new HashMap<>();
+        sdMetadata.put("displayName", "MongoDB");
+        sdMetadata.put("imageUrl",
+                "http://info.mongodb.com/rs/mongodb/images/MongoDB_Logo_Full.png");
+        sdMetadata.put("longDescription", "MongoDB Service");
+        sdMetadata.put("providerDisplayName", "Pivotal");
+        sdMetadata.put("documentationUrl",
+                "https://github.com/srinivasa-vasu/cloudfoundry-service-broker");
+        sdMetadata.put("supportUrl",
+                "https://github.com/srinivasa-vasu/cloudfoundry-service-broker");
+        return sdMetadata;
+    }
+
 }
--- src/main/java/org/springframework/cloud/servicebroker/mongodb/model/ServiceInstanceParams.java
+++ src/main/java/org/springframework/cloud/servicebroker/mongodb/model/ServiceInstanceParams.java
@@ -1,5 +1,11 @@
 package org.springframework.cloud.servicebroker.mongodb.model;
 
+import static org.springframework.cloud.servicebroker.mongodb.config.CatalogConfig.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.cloud.servicebroker.exception.ServiceBrokerException;
 import org.springframework.cloud.servicebroker.model.CreateServiceInstanceRequest;
 import org.springframework.cloud.servicebroker.mongodb.config.MongoConfig;
@@ -44,6 +50,8 @@ public class ServiceInstanceParams {
 	private String url;
 	private int exposePort = 31000;
 	private long serviceTimeout = 30;
+	private String storage;
+	private int replicas = 1;
 
 	/**
 	 * Create a ServiceInstanceParams from a create request. If fields are not present in
@@ -51,9 +59,10 @@ public class ServiceInstanceParams {
 	 * @param request containing details of ServiceInstance
 	 */
 	public ServiceInstanceParams(CreateServiceInstanceRequest request,
-                                 MongoConfig config) {
+			MongoConfig config) {
 		initialize(config);
 		populate(request);
+		populatePlanParams(request);
 		validateInputParams(request);
 	}
 
@@ -91,6 +100,18 @@ public class ServiceInstanceParams {
 				break;
 			}
 		}
+	}
+
+	private void populatePlanParams(CreateServiceInstanceRequest request) {
+		request.getServiceDefinition().getPlans().stream()
+				.filter(plan -> plan.getId().equalsIgnoreCase(request.getPlanId()))
+				.findFirst()
+				.ifPresent(plan -> ((List<Map<String, Object>>) plan.getMetadata()
+						.getOrDefault(COSTS, new HashMap<>())).forEach(key -> {
+							setStorage((String) key.getOrDefault(STORAGE, "128Mi"));
+							setReplicas(Integer
+									.valueOf((String) key.getOrDefault(REPLICAS, "1")));
+						}));
 	}
 
 	private void validateInputParams(CreateServiceInstanceRequest request) {
@@ -165,6 +186,22 @@ public class ServiceInstanceParams {
 		return exposePort;
 	}
 
+	public String getStorage() {
+		return storage;
+	}
+
+	private void setStorage(String storage) {
+		this.storage = storage;
+	}
+
+	public int getReplicas() {
+		return replicas;
+	}
+
+	private void setReplicas(int replicas) {
+		this.replicas = replicas;
+	}
+
 	@Override
 	public String toString() {
 		final StringBuilder sb = new StringBuilder("ServiceInstanceParams{");
@@ -174,6 +211,8 @@ public class ServiceInstanceParams {
 		sb.append(", url='").append(url).append('\'');
 		sb.append(", exposePort=").append(exposePort);
 		sb.append(", serviceTimeout=").append(serviceTimeout);
+		sb.append(", storage='").append(storage).append('\'');
+		sb.append(", replicas=").append(replicas);
 		sb.append('}');
 		return sb.toString();
 	}
